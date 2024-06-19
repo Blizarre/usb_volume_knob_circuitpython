decc816edb3b759f88c1829559238db4df918150
Simon Marache <github@simon.marache.net>
2024-06-19 11:44:25 +0100

    boot should release the playpause button at the end

--- boot.py
+++ boot.py
@@ -6,16 +6,16 @@ import usb_hid
 import pins
 
 # disable all the extra USB peripherals UNLESS the playpause button is pressed at boot time
-button = digitalio.DigitalInOut(pins.PLAYPAUSE)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+with digitalio.DigitalInOut(pins.PLAYPAUSE) as button:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
 
-# Disable devices only if button is not pressed (default UP).
-if button.value:
-    storage.disable_usb_drive()
-    usb_cdc.disable()
-    usb_midi.disable()
+    # Disable devices only if button is not pressed (default UP).
+    if button.value:
+        storage.disable_usb_drive()
+        usb_cdc.disable()
+        usb_midi.disable()
 
-    usb_hid.enable(
-        (usb_hid.Device.CONSUMER_CONTROL,)
-    )  # Do not enable any of the other hid
+        usb_hid.enable(
+            (usb_hid.Device.CONSUMER_CONTROL,)
+        )  # Do not enable any of the other hid
